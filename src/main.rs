943658abb590904429be90308a83aeab5a2aac0e
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-06-24 18:44:42 -0300

    add system setup settings

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #[allow(unused, dead_code)]
 use bevy::prelude::*;
 
+const PADDLE_START_Y: f32 = 0.0;
+const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Update, bevy::window::close_on_esc)
+        .add_systems(Startup, setup)
         .run();
 
-    
+
+}
+
+#[derive(Component)]
+struct Paddle;
+
+fn setup() {
 }
