ba98352e0ab944376977768a24de6567b6d548cb
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-06-26 15:02:37 -0300

    spawn method for buddle and player

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #[allow(unused, dead_code)]
+
+
+use bevy::math::vec3;
 use bevy::prelude::*;
 
 const PADDLE_START_Y: f32 = 0.0;
 const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-
 
 fn main() {
     App::new()
@@ -12,12 +14,29 @@ fn main() {
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
         .run();
-
-
 }
 
 #[derive(Component)]
 struct Paddle;
 
-fn setup() {
+fn setup(mut commands: Commands) {
+    //camera
+    commands.spawn(Camera2dBundle::default());
+
+    //paddle
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: vec3(0., PADDLE_START_Y, 0.),
+                ..default()
+            },
+            sprite: Sprite {
+                color: PADDLE_COLOR,
+                custom_size: Some(PADDLE_SIZE),
+                ..default()
+            },
+            ..default()
+        },
+        Paddle,
+    ));
 }
