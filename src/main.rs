7292d075f8fde9088a9d7ec615c7fefcec85746d
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-06-26 15:40:34 -0300

    ball variables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
 #[allow(unused, dead_code)]
-
-
 use bevy::math::vec3;
 use bevy::prelude::*;
 
+//paddle
 const PADDLE_START_Y: f32 = 0.0;
 const PADDLE_SIZE: Vec2 = Vec2::new(120.0, 20.0);
 const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
 const PADDLE_SPEED: f32 = 500.0;
 
+//ball
+const BALL_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, -50.0, 1.0);
+const BALL_SIZE: Vec2 = Vec2::new(30.0, 30.0);
+const BALL_SPEED: f32 = 400.0;
+const BALL_INITIAL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb( 0.9, 0.9, 0.9)))
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
         .add_systems(FixedUpdate, move_paddle)
@@ -47,7 +53,7 @@ fn setup(mut commands: Commands) {
 fn move_paddle(
     input: Res<Input<KeyCode>>,
     time_step: Res<FixedTime>,
-    mut query: Query<&mut Transform, With<Paddle>>
+    mut query: Query<&mut Transform, With<Paddle>>,
 ) {
     let mut paddle_transform = query.single_mut();
     let mut direction_x = 0.0;
@@ -60,7 +66,8 @@ fn move_paddle(
         direction_x += 1.0;
     }
 
-    let new_x = paddle_transform.translation.x + direction_x * PADDLE_SPEED * time_step.period.as_secs_f32();
+    let new_x = paddle_transform.translation.x
+        + direction_x * PADDLE_SPEED * time_step.period.as_secs_f32();
 
     paddle_transform.translation.x = new_x;
 
@@ -75,7 +82,8 @@ fn move_paddle(
         direction_y += 1.0;
     }
 
-    let new_y = paddle_transform.translation.y + direction_y * PADDLE_SPEED * time_step.period.as_secs_f32();
+    let new_y = paddle_transform.translation.y
+        + direction_y * PADDLE_SPEED * time_step.period.as_secs_f32();
 
     paddle_transform.translation.y = new_y;
 }
