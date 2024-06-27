a9472833aeb3dacedbd5bf603bca9646f54e88ee
Victor Giuliano <vitor.giuliano@gmail.com>
2024-06-27 17:11:05 -0300

    scoreboard struct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-
 #[allow(unused, dead_code)]
-
 mod constants;
 
-use bevy::{math::*, prelude::*, render::{settings::{Backends, WgpuSettings}, RenderPlugin}, sprite::collide_aabb::*};
+use bevy::{
+    math::*,
+    prelude::*,
+    render::{
+        settings::{Backends, WgpuSettings},
+        RenderPlugin,
+    },
+    sprite::collide_aabb::*, transform::commands,
+};
 use constants::constants::*;
-
 
 fn main() {
     App::new()
@@ -16,6 +21,7 @@ fn main() {
             },
         }))
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
+        .insert_resource(Scoreboard { score: 0})
         .add_systems(Update, bevy::window::close_on_esc)
         .add_systems(Startup, setup)
         .add_systems(
@@ -53,6 +59,11 @@ struct WallBundle {
 
 #[derive(Component)]
 struct Brick;
+
+#[derive(Resource, Clone, Copy)]
+struct Scoreboard {
+    score: usize
+}
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     //camera
@@ -187,28 +198,35 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             let offset_y = BOTTOM_WALL + GAP_BETWEEN_PADDLE_AND_BRICKS + BRICK_SIZE.y * 0.5;
 
             let bricks_total_width = (RIGHT_WALL - LEFT_WALL) - 2. * GAP_BETWEEN_BRICKS_AND_SIDES;
-            let bricks_total_height = (TOP_WALL - BOTTOM_WALL) - GAP_BETWEEN_BRICKS_AND_CEILING - GAP_BETWEEN_PADDLE_AND_BRICKS;
+            let bricks_total_height = (TOP_WALL - BOTTOM_WALL)
+                - GAP_BETWEEN_BRICKS_AND_CEILING
+                - GAP_BETWEEN_PADDLE_AND_BRICKS;
 
             let rows = (bricks_total_height / (BRICK_SIZE.y + GAP_BETWEEN_BRICKS)).floor() as i32;
-            let columns = (bricks_total_width / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)). floor() as i32;
+            let columns = (bricks_total_width / (BRICK_SIZE.x + GAP_BETWEEN_BRICKS)).floor() as i32;
             //convert values to i32 its just necessary to for loops ahead
 
             for row in 0..rows {
                 for column in 0..columns {
                     let brick_pos = vec2(
-                        offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS), 
+                        offset_x + column as f32 * (BRICK_SIZE.x + GAP_BETWEEN_BRICKS),
                         offset_y + row as f32 * (BRICK_SIZE.y + GAP_BETWEEN_BRICKS),
                     );
 
                     commands.spawn((
-                        SpriteBundle {}
-                    ))
-
+                        SpriteBundle {
+                            transform: Transform {
+                                translation: brick_pos.extend(0.0),
+                                ..default()
+                            },
+                            ..default()
+                            Brick,
+                            Collider { size: BRICK_SIZE },
+                        },
+                    ));
                 }
             }
-
         }
-
     }
 }
 
@@ -271,11 +289,13 @@ fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>, time_setp: Res<
 }
 
 fn check_ball_collisions(
+    mut commands: Commands,
+    mut score: ResMut<Scoreboard>,
     mut ball_query: Query<(&mut Velocity, &Transform, &Ball)>,
-    collider_query: Query<(&Transform, &Collider)>,
+    collider_query: Query<(Entity, &Transform, &Collider, Option<&Brick>)>,
 ) {
     for (mut ball_velocity, ball_transform, ball) in &mut ball_query {
-        for (transform, other) in &collider_query {
+        for (other_entity, transform, other, opt_brick) in &collider_query {
             let collision = collide(
                 ball_transform.translation,
                 ball.size,
@@ -300,6 +320,12 @@ fn check_ball_collisions(
                 }
                 if reflect_y {
                     ball_velocity.y *= -1.;
+                }
+
+                if  opt_brick.is_some() {
+                    score.score += 1;
+                    println!("Score: {}", score.score);
+                    commands.entity(other_entity).despawn()
                 }
             }
         }
